53a596b91df3eb0a48708e493dbb46fe34570d24
megalon <megalongaming@gmail.com>
2019-04-23 01:49:12 -0700

    Revert "Even more debug info"
    This reverts commit 621855cb6e64b8aa9088ed36e6177d2a601f9a0b.

--- BeatSaberModManager/Core/RemoteLogic.cs
+++ BeatSaberModManager/Core/RemoteLogic.cs
@@ -63,7 +63,6 @@ namespace BeatSaberModManager.Core
             string raw = GetBeatModsReleases();
             if (raw != null)
             {
-                var scoreSaberFound = false;
                 var mods = JSON.Parse(raw);
                 for (int i = 0; i < mods.Count; i++)
                 {
@@ -73,8 +72,6 @@ namespace BeatSaberModManager.Core
                     List<ModLink> conflictsWith = NodeToLinks(current["conflicts"]);
 
                     var files = current["downloads"];
-
-                    if (current["name"] == "ScoreSaber") scoreSaberFound = true;
 
                     for (var f = 0; f < files.Count; ++f)
                     {
@@ -122,11 +119,6 @@ namespace BeatSaberModManager.Core
                             current["description"], current["link"], 0, "0.13.2", files[0]["url"],
                             current["category"], Platform.Default, dependsOn, conflictsWith));
                     }
-                }
-
-                if (!scoreSaberFound)
-                {
-                    MessageBox.Show($"ScoreSaber was not found in BeatMods API result!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
         }
